71bf04e521ad08225ed03e025c597965d72a9301
lucas-the-wise-dreams <lucas@internship.thewisedreams.com>
2024-05-15 13:27:42 +0100

    Home y Login

---
+++ FrontEnd/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import { Navbar } from "../../components/Navbar";
+
+export default function Home() {
+
+
+  return (
+    <>
+      <Navbar />
+      <div className="flex gap-3 m-5 flex-wrap justify-center"></div>
+    </>
+  );
+}
