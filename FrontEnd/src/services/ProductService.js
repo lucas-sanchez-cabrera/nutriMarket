17f84db764b1ad74bed32ae73bd4fd8b402ab658
lucas-the-wise-dreams <lucas@internship.thewisedreams.com>
2024-05-17 15:30:23 +0100

    añadi Card y Modal de productos, muestra las tarjetas y toda la vaina

--- FrontEnd/src/services/ProductService.js
+++ FrontEnd/src/services/ProductService.js
@@ -2,29 +2,31 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:8080/producto';
 
-export async function createProduct(product) {
+async function createProduct(product) {
     return await axios.post(API_URL, product);
 }
 
-export async function getProducts() {
+async function getProducts() {
     return await axios.get(API_URL);
 }
 
-export async function getProductById(productId) {
+async function getProductById(productId) {
     return await axios.get(`${ API_URL }/${ productId }`);
 }
 
-export async function getProductByName(productName) {
+async function getProductByName(productName) {
     return await axios.get(`${ API_URL }/${ productName }`);
 }
-
-export async function getProductByCategory(productCategory) {
+async function getProductByCategory(productCategory) {
     return await axios.get(`${ API_URL }/, ${ productCategory }`);
 }
 
-export async function updateProduct(productId, productMod) {
+async function updateProduct(productId, productMod) {
     return await axios.post(`${ API_URL }/update/${ productId }`, productMod);
 }
-export async function deleteProduct(productId) {
+
+async function deleteProduct(productId) {
     return await axios.delete(`${ API_URL }/delete/${ productId }`);
 }
+
+export default {createProduct, getProducts, getProductByCategory, getProductById, getProductByName, deleteProduct, updateProduct };
